1b581675675eb6b9ca9792fc0d179f3db7338da1
Clarisse U <clarisseum@gmail.com>
2019-07-18 19:58:27 +0200

    make choice

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ function wd() {
 
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var z = document.getElementById('choice').value;
+
+    if (z === 'female') {
+        console.log(female[Math.round(weekDay)]);
+        document.getElementById('akanName').innerHTML = female[Math.round(weekDay)];
+    } else if (choice === 'female') {
+        console.log(male[Math.round(weekDay)]);
+        document.getElementById('akanName').innerHTML = male[Math.round(weekDay)];
+    } else {
+        console.log("invalid output");
+    }
 }
