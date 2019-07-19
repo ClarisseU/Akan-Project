e629dacff5c62f421c7e26b2725f55d990367065
Clarisse U <clarisseum@gmail.com>
2019-07-19 18:53:12 +0200

    changes

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function wd() {
-    var d = parseFloat(document.getElementById('day').value);
-    var m = parseFloat(document.getElementById('month').value);
-    var y = parseFloat(document.getElementById('year').value);
-    var x = (y - 1) / 100 + 1;
-    var weekDay = (((c / 4) - 2 * c - 1) + ((5 * y / 4)) + ((26 * (m + 1) / 10)) + d) % 7;
-    console.log(weekDay);
-    document.getElementById('result').innerHTML = Math.round(weekDay);
 
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var d = parseFloat(document.getElementById('a').value);
+    var m = parseFloat(document.getElementById('b').value);
+    var y = parseFloat(document.getElementById('c').value);
+    var c = (y - 1) / 100 + 1;
+    var weekDay = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
+    //alert(weekDay);
+    console.log(weekDay);
+    document.getElementById('answer').innerHTML = Math.round(weekDay);
+
+    var femme = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var homme = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var z = document.getElementById('choice').value;
 
-    if (z === 'female') {
-        console.log(female[Math.round(weekDay)]);
-        document.getElementById('akanName').innerHTML = female[Math.round(weekDay)];
-    } else if (choice === 'female') {
-        console.log(male[Math.round(weekDay)]);
-        document.getElementById('akanName').innerHTML = male[Math.round(weekDay)];
+    if (z === "female") {
+        console.log(femme[Math.round(weekDay)]);
+        document.getElementById('akanName').innerHTML = femme[Math.round(weekDay)];
+    } else if (z === "male") {
+        console.log(homme[Math.round(weekDay)]);
+        document.getElementById('akanName').innerHTML = homme[Math.round(weekDay)];
     } else {
         console.log("invalid output");
     }
