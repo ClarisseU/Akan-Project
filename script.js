78931dfa64db1542a78ae119411ffd388e93c53d
Clarisse U <clarisseum@gmail.com>
2019-07-22 09:50:25 +0200

    edit script

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function wd() {
     var c = (y - 1) / 100 + 1;
     var weekDay = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
     console.log(weekDay);
-    document.getElementById('answer').innerHTML = Math.round(weekDay);
+
 
     var femme = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var homme = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
